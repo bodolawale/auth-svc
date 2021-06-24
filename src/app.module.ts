c0286b099107b9723482f12eebb6982c7e033e6b
bodolawale <bodolawale@gmail.com>
2021-06-24 16:42:11 +0100

    added env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController, AuthController],
   providers: [AppService, AuthService],
 })
